1c84ec02964d4f0ef5731f7ff09ee8aa58e7032f
Enzo Brasil <enzobrasil1207@gmail.com>
2025-05-23 19:12:33 -0300

    commit teste

---
+++ teste.js
@@ -0,0 +1,6 @@
+//    var prompt= function(t){return lines.shift()}
+var x = parseInt(prompt("Digite o valor de X:"));
+var Letras_Placas = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,Y,X,Z]
+for(let i = 1; i<=3; i++){
+
+}
